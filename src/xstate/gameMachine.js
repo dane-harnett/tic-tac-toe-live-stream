79c9aa64dd6ba9e1dcc5d3c42a64a2e36b0a3705
Dane Harnett <dane_harnett@flightcentre.com>
2020-07-01 12:26:03 +1000

    Extract actions into options

--- src/xstate/gameMachine.js
+++ src/xstate/gameMachine.js
@@ -1,47 +1,55 @@
 import { assign, Machine } from "xstate";
 
-const gameMachine = Machine({
-  id: "game",
-  initial: "player1",
-  context: {
-    gameBoard: [
-      ["", "", ""],
-      ["", "", ""],
-      ["", "", ""],
-    ],
-  },
-  states: {
-    player1: {
-      on: {
-        PLAY: {
-          actions: assign({
-            gameBoard: (context, event) => {
-              const newGameBoard = [...context.gameBoard];
-              newGameBoard[event.row][event.col] = "0";
-              return newGameBoard;
-            },
-          }),
-          target: "player2",
+const gameMachine = Machine(
+  {
+    id: "game",
+    initial: "player1",
+    context: {
+      gameBoard: [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""],
+      ],
+    },
+    states: {
+      player1: {
+        on: {
+          PLAY: {
+            actions: "player1Turn",
+            target: "player2",
+          },
         },
       },
-    },
-    player2: {
-      on: {
-        PLAY: {
-          actions: assign({
-            gameBoard: (context, event) => {
-              const newGameBoard = [...context.gameBoard];
-              newGameBoard[event.row][event.col] = "X";
-              return newGameBoard;
-            },
-          }),
-          target: "player1",
+      player2: {
+        on: {
+          PLAY: {
+            actions: "player2Turn",
+            target: "player1",
+          },
         },
       },
+      winner: {},
+      tie: {},
     },
-    winner: {},
-    tie: {},
   },
-});
+  {
+    actions: {
+      player1Turn: assign({
+        gameBoard: (context, event) => {
+          const newGameBoard = [...context.gameBoard];
+          newGameBoard[event.row][event.col] = "0";
+          return newGameBoard;
+        },
+      }),
+      player2Turn: assign({
+        gameBoard: (context, event) => {
+          const newGameBoard = [...context.gameBoard];
+          newGameBoard[event.row][event.col] = "X";
+          return newGameBoard;
+        },
+      }),
+    },
+  }
+);
 
 export default gameMachine;
